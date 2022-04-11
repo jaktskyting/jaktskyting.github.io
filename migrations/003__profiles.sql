bd76552c60d399e1bab4d7d3429349d4d4ddfba2
Eirik Meland <eirik.meland@schibsted.com>
2022-04-11 22:57:34 +0200

    migrations

---
+++ migrations/003__profiles.sql
@@ -0,0 +1,29 @@
+-- auto-generated definition
+create table profiles
+(
+    id         uuid not null
+        constraint profiles_pkey
+            primary key
+        constraint profiles_id_fkey
+            references auth.users,
+    updated_at timestamp with time zone,
+    username   text
+        constraint profiles_username_key
+            unique
+        constraint username_length
+            check (char_length(username) >= 3),
+    avatar_url text,
+    website    text
+);
+
+alter table profiles
+    owner to supabase_admin;
+
+grant delete, insert, references, select, trigger, truncate, update on profiles to postgres;
+
+grant delete, insert, references, select, trigger, truncate, update on profiles to anon;
+
+grant delete, insert, references, select, trigger, truncate, update on profiles to authenticated;
+
+grant delete, insert, references, select, trigger, truncate, update on profiles to service_role;
+
